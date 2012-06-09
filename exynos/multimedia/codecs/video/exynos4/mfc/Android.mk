4ca7aa57e3df01b8fb357b2f11b62d0fbc723882
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-06-09 11:37:41 +0200

    [WIP] exynos: updated mfc api for exynos4x12
    Change-Id: I67d4599204edfbd89ee9994d494e7740f9d01464

--- exynos/multimedia/codecs/video/exynos4/mfc/Android.mk
+++ exynos/multimedia/codecs/video/exynos4/mfc/Android.mk
@@ -1,5 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
+#
+# mfc combined api
+#
+
 include $(CLEAR_VARS)
 
 LOCAL_COPY_HEADERS_TO := libsecmm
@@ -8,6 +12,7 @@ LOCAL_COPY_HEADERS := \
 	include/mfc_interface.h \
 	include/SsbSipMfcApi.h
 
+LOCAL_MODULE := libsecmfcapi
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := \
@@ -20,8 +25,16 @@ LOCAL_C_INCLUDES := \
 LOCAL_PRELINK_MODULE := false
 
 ifeq ($(BOARD_USES_MFC_FPS),true)
-LOCAL_CFLAGS := -DCONFIG_MFC_FPS
+LOCAL_CFLAGS += -DCONFIG_MFC_FPS
 #LOCAL_CFLAGS += -DCONFIG_MFC_PERF_LOG
+endif
+
+ifeq ($(TARGET_SOC),exynos4210)
+LOCAL_CFLAGS += -DSAMSUNG_EXYNOS4210
+endif
+
+ifeq ($(TARGET_SOC),exynos4x12)
+LOCAL_CFLAGS += -DSAMSUNG_EXYNOS4x12
 endif
 
 ifeq ($(BOARD_USE_S3D_SUPPORT), true)
@@ -33,10 +46,98 @@ LOCAL_ARM_MODE := arm
 LOCAL_STATIC_LIBRARIES :=
 LOCAL_SHARED_LIBRARIES := liblog
 
-ifeq ($(TARGET_SOC),exynos4x12)
-LOCAL_MODULE := libsecmfcdecapi
-include $(BUILD_SHARED_LIBRARY)
-else
-LOCAL_MODULE := libsecmfcapi
 include $(BUILD_STATIC_LIBRARY)
+
+#
+# mfc dec api
+#
+
+include $(CLEAR_VARS)
+
+LOCAL_COPY_HEADERS_TO := libsecmm
+LOCAL_COPY_HEADERS := \
+	include/mfc_errno.h \
+	include/mfc_interface.h \
+	include/SsbSipMfcApi.h
+
+LOCAL_MODULE := libsecmfcdecapi
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := \
+	dec/src/SsbSipMfcDecAPI.c
+
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/include/
+
+LOCAL_PRELINK_MODULE := false
+
+ifeq ($(BOARD_USES_MFC_FPS),true)
+LOCAL_CFLAGS += -DCONFIG_MFC_FPS
+#LOCAL_CFLAGS += -DCONFIG_MFC_PERF_LOG
 endif
+
+ifeq ($(TARGET_SOC),exynos4210)
+LOCAL_CFLAGS += -DSAMSUNG_EXYNOS4210
+endif
+
+ifeq ($(TARGET_SOC),exynos4x12)
+LOCAL_CFLAGS += -DSAMSUNG_EXYNOS4x12
+endif
+
+ifeq ($(BOARD_USE_S3D_SUPPORT), true)
+LOCAL_CFLAGS += -DS3D_SUPPORT
+endif
+
+LOCAL_ARM_MODE := arm
+
+LOCAL_STATIC_LIBRARIES :=
+LOCAL_SHARED_LIBRARIES := liblog
+
+include $(BUILD_SHARED_LIBRARY)
+
+#
+# mfc enc api
+#
+
+include $(CLEAR_VARS)
+
+LOCAL_COPY_HEADERS_TO := libsecmm
+LOCAL_COPY_HEADERS := \
+	include/mfc_errno.h \
+	include/mfc_interface.h \
+	include/SsbSipMfcApi.h
+
+LOCAL_MODULE := libsecmfcencapi
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := \
+	enc/src/SsbSipMfcEncAPI.c
+
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/include/
+
+LOCAL_PRELINK_MODULE := false
+
+ifeq ($(BOARD_USES_MFC_FPS),true)
+LOCAL_CFLAGS += -DCONFIG_MFC_FPS
+#LOCAL_CFLAGS += -DCONFIG_MFC_PERF_LOG
+endif
+
+ifeq ($(TARGET_SOC),exynos4210)
+LOCAL_CFLAGS += -DSAMSUNG_EXYNOS4210
+endif
+
+ifeq ($(TARGET_SOC),exynos4x12)
+LOCAL_CFLAGS += -DSAMSUNG_EXYNOS4x12
+endif
+
+ifeq ($(BOARD_USE_S3D_SUPPORT), true)
+LOCAL_CFLAGS += -DS3D_SUPPORT
+endif
+
+LOCAL_ARM_MODE := arm
+
+LOCAL_STATIC_LIBRARIES :=
+LOCAL_SHARED_LIBRARIES := liblog
+
+include $(BUILD_SHARED_LIBRARY)
