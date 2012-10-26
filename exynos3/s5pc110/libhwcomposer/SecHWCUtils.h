c07fee0b81e9d8f8f9b8cc4ce06e85b490b44dfc
Petr Havlena <havlenapetr@gmail.com>
2012-10-26 17:52:00 +0530

    exynos3: hwc: add hdmi support
    Change-Id: I9def46f17fc2b3042af776b253ac3441fd6d3405

--- exynos3/s5pc110/libhwcomposer/SecHWCUtils.h
+++ exynos3/s5pc110/libhwcomposer/SecHWCUtils.h
@@ -37,6 +37,9 @@
 #include <hardware/gralloc.h>
 #include <hardware/hardware.h>
 #include <hardware/hwcomposer.h>
+#if defined(BOARD_USES_HDMI)
+#include "hardware/hdmi.h"
+#endif
 
 #include "s5p_fimc.h"
 #include "sec_lcd.h"
@@ -121,6 +124,9 @@ struct hwc_context_t {
     unsigned int              num_of_fb_layer;
     unsigned int              num_of_hwc_layer;
     unsigned int              num_of_fb_layer_prev;
+#if defined(BOARD_USES_HDMI)
+    hdmi_device_t*            hdmi;
+#endif
 };
 
 int window_open(struct hwc_win_info_t *win, int id);
