2c818225252c98681de70109d8f4d6657c486f09
Chirayu Desai <cdesai@cyanogenmod.org>
2012-11-16 14:32:19 +0530

    exynos5: remove sources
    - upstream has them at hardware/samsung_slsi
    
    Change-Id: I6721098016ea7d760b553d533613ffc450eefd86

--- exynos/multimedia/utils/csc/Android.mk
+++ exynos/multimedia/utils/csc/Android.mk
@@ -5,7 +5,3 @@ include $(CLEAR_VARS)
 ifeq ($(filter-out exynos4,$(TARGET_BOARD_PLATFORM)),)
 include   $(LOCAL_PATH)/exynos4/Android.mk
 endif
-
-ifeq ($(filter-out exynos5,$(TARGET_BOARD_PLATFORM)),)
-include   $(LOCAL_PATH)/exynos5/Android.mk
-endif
