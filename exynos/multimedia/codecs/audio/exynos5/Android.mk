02bd4ba41fd8512482c6648bc91054c530aa04fb
Chirayu Desai <cdesai@cyanogenmod.org>
2012-11-16 14:32:19 +0530

    exynos5: remove sources
    - upstream has them at hardware/samsung_slsi
    
    Change-Id: Ib2b1e674d4505703d8fe68c177c4c9d7039939fc
    Signed-off-by: Chirayu Desai <cdesai@cyanogenmod.org>

--- exynos/multimedia/codecs/audio/exynos5/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_AUDIO_PATH :=$(LOCAL_PATH)
-
-ifeq ($(BOARD_USE_ALP_AUDIO), true)
-  include $(LOCAL_AUDIO_PATH)/srp/alp/Android.mk
-endif
