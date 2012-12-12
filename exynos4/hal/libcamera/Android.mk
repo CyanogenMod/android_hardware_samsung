c3c833ec148b5295f126a60db33a46a6bfb54af1
XpLoDWilD <xplodgui@gmail.com>
2012-12-12 16:00:31 +0000

    exynos4: hal: Update open-source libcamera, libfimc, libhwjpeg
    Update 12-12 22:30 GMT+1
    ================================
    Adapted for i9300 sensor (not sure what other smdk4412-based devices uses).
    Video recording still fine, still cannot take a proper shot. Output file
    seems to contains something that looks like JPEG data, but no image
    software can read it.
    Updated videodev2_samsung based on the file in kernel sources.
    
    Change-Id: Iba11e7178703ce2f7a07fa895bce8870b4cb33bc

--- exynos4/hal/libcamera/Android.mk
+++ exynos4/hal/libcamera/Android.mk
@@ -7,10 +7,15 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include
 
+ifeq ($(CAMERA_USE_DIGITALZOOM), true)
+LOCAL_SRC_FILES:= \
+	SecCamera_zoom.cpp SecCameraHWInterface_zoom.cpp
+else
 LOCAL_SRC_FILES:= \
 	SecCamera.cpp SecCameraHWInterface.cpp
+endif
 
-LOCAL_SHARED_LIBRARIES:= libutils libcutils libbinder liblog libcamera_client libhardware
+LOCAL_SHARED_LIBRARIES:= libutils libcutils libbinder liblog libcamera_client libhardware libswscaler
 
 ifeq ($(TARGET_SOC), exynos4210)
 LOCAL_SHARED_LIBRARIES += libs5pjpeg
@@ -18,20 +23,11 @@ LOCAL_CFLAGS += -DSAMSUNG_EXYNOS4210
 endif
 
 ifeq ($(TARGET_SOC), exynos4x12)
-LOCAL_SHARED_LIBRARIES += libhwjpeg
+LOCAL_SHARED_LIBRARIES += libhwjpeg libs5pjpeg
 LOCAL_CFLAGS += -DSAMSUNG_EXYNOS4x12
 endif
 
-ifeq ($(BOARD_USE_V4L2), true)
-LOCAL_CFLAGS += -DBOARD_USE_V4L2
-endif
-
-ifeq ($(BOARD_USE_V4L2_ION), true)
-LOCAL_CFLAGS += -DBOARD_USE_V4L2
-LOCAL_CFLAGS += -DBOARD_USE_V4L2_ION
-endif
-
-LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := camera.$(TARGET_DEVICE)
 
 LOCAL_MODULE_TAGS := optional
 
