b4264059d11959c9ef702c362a960f3fed1e1ffb
Brint E. Kriebel <bkriebel@vmware.com>
2012-12-18 17:39:20 -0700

    exynos4: Set libgralloc_ump and SecHdmi tags to optional
    When LOCAL_MODULE_TAGS was set to eng this broke eng builds
    due to the lack of source for libsamsungion. Set the tag to
    optional so it is not built for an eng build.
    
    Change-Id: I770c991dc16ccce9905501619e015567110de577

--- exynos4/hal/libhdmi/SecHdmi/Android.mk
+++ exynos4/hal/libhdmi/SecHdmi/Android.mk
@@ -17,7 +17,7 @@ ifeq ($(BOARD_USES_HDMI),true)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_TAGS := optional
 
 LOCAL_PRELINK_MODULE := false
 #LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
