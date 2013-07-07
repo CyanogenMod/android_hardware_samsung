c99233950b5cb2435a3ad0483bf57860d2cff2f8
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-07-07 10:06:59 +0200

    samsung: add libril for xmm626x modems
    Change-Id: I3064271ac8d563f63b13278944639ab5a29ae87f

---
+++ ril/xmm6260/libril/Android.mk
@@ -0,0 +1,47 @@
+# Copyright 2006 The Android Open Source Project
+
+ifeq ($(BOARD_PROVIDES_LIBRIL),true)
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+    ril.cpp \
+    ril_event.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libutils \
+    libbinder \
+    libcutils \
+    libhardware_legacy
+
+LOCAL_CFLAGS :=
+
+LOCAL_MODULE:= libril
+
+LOCAL_LDLIBS += -lpthread
+
+include $(BUILD_SHARED_LIBRARY)
+
+
+# For RdoServD which needs a static library
+# =========================================
+ifneq ($(ANDROID_BIONIC_TRANSITION),)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+    ril.cpp
+
+LOCAL_STATIC_LIBRARIES := \
+    libutils_static \
+    libcutils
+
+LOCAL_CFLAGS :=
+
+LOCAL_MODULE:= libril_static
+
+LOCAL_LDLIBS += -lpthread
+
+include $(BUILD_STATIC_LIBRARY)
+endif # ANDROID_BIONIC_TRANSITION
+endif # BOARD_PROVIDES_LIBRIL
