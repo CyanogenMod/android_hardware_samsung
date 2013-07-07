601dc8581733786120d590f2c8797f4feeb84b44
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-07-07 10:06:59 +0200

    samsung: add libril for xmm626x modems
    Change-Id: I3064271ac8d563f63b13278944639ab5a29ae87f

---
+++ ril/Android.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2013 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(BOARD_VENDOR),samsung)
+ifeq ($(BOARD_PROVIDES_LIBRIL),true)
+
+ifeq ($(BOARD_MODEM_TYPE),xmm6260)
+include $(LOCAL_PATH)/xmm6260/libril/Android.mk
+endif
+
+ifeq ($(BOARD_MODEM_TYPE),xmm6262)
+include $(LOCAL_PATH)/xmm6262/libril/Android.mk
+endif
+
+endif
+endif
+
