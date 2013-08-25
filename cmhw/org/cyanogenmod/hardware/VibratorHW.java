f138a2fb672039bb979e69d75e5e74404377daf5
David Daynard <nardholio@gmail.com>
2013-08-25 09:26:49 -0400

    vibrator intensity: disable tunable framework if not supported
    Change-Id: I6481b82b7ee5412234d42e39f7f5560489f14af3

--- cmhw/org/cyanogenmod/hardware/VibratorHW.java
+++ cmhw/org/cyanogenmod/hardware/VibratorHW.java
@@ -29,7 +29,13 @@ public class VibratorHW {
     private static String LEVEL_THRESHOLD_PATH = "/sys/class/timed_output/vibrator/pwm_threshold";
 
     public static boolean isSupported() {
-        return true;
+        File f = new File(LEVEL_PATH);
+
+        if(f.exists()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static int getMaxIntensity()  {
