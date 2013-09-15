12de32d39e46982fc3ce16d790e3e516c6ed0b10
Joel Teichroeb <joel@teichroeb.net>
2013-09-15 12:01:11 -0700

    exynos3: sec_mm: Allow each target device to set its own omx mmap buffer size
    Change-Id: I93474ea1e92820b02a8d3c4dae80d1a42a90498a

--- exynos3/s5pc110/sec_mm/sec_omx/sec_codecs/Android.mk
+++ exynos3/s5pc110/sec_mm/sec_omx/sec_codecs/Android.mk
@@ -6,6 +6,3 @@ include $(SEC_CODECS)/video/mfc_c110/dec/Android.mk
 include $(SEC_CODECS)/video/mfc_c110/enc/Android.mk
 include $(SEC_CODECS)/video/mfc_c110/csc/Android.mk
 
-ifneq ($(TARGET_SEC_OMX_BIG_MMAP_BUFFER_SIZE),true)
-    LOCAL_CFLAGS += -DBIG_MMAP_BUFFER_SIZE
-endif
