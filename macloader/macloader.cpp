037879359fe52c28fbc9ef445ce9b05ea31db5df
Andreas Schneider <asn@cryptomilk.org>
2014-11-19 11:37:09 +0100

    macloader: Add SEMCO3RD and WISOL for BCM4354.
    Change-Id: I08f407297a974ec87ae51e4580d1c41a380a46d0

--- macloader/macloader.cpp
+++ macloader/macloader.cpp
@@ -33,7 +33,9 @@ enum Type {
     NONE,
     MURATA,
     SEMCOSH,
-    SEMCOVE
+    SEMCOVE,
+    SEMCO3RD,
+    WISOL
 };
 
 int main() {
@@ -92,6 +94,16 @@ int main() {
         type = SEMCOSH;
     }
 
+    /* semco3rd */
+    if (strncasecmp(mac_addr_half, "f4:09:d8", 9) == 0) {
+        type = SEMCO3RD;
+    }
+
+    /* wisol */
+    if (strncasecmp(mac_addr_half, "48:5A:3F", 9) == 0) {
+        type = WISOL;
+    }
+
     if (type != NONE) {
         /* open cid file */
         cidfile = fopen(CID_PATH, "w");
@@ -119,6 +131,14 @@ int main() {
                 /* write semcove to cid file */
                 ALOGI("Writing semcove to %s\n", CID_PATH);
                 ret = fputs("semcove", cidfile);
+            break;
+            case SEMCO3RD:
+                ALOGI("Writing semco3rd to %s\n", CID_PATH);
+                ret = fputs("semco3rd", cidfile);
+            break;
+            case WISOL:
+                ALOGI("Writing wisol to %s\n", CID_PATH);
+                ret = fputs("wisol", cidfile);
             break;
          }
 
