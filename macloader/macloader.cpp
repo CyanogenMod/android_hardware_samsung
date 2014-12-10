d426a9286d0b7538357d5b45b8d5fdb9d6c67be4
MarcKe <herderkewitz@googlemail.com>
2014-12-10 19:12:54 +0100

    macloader: add semcosh mac address
    Change-Id: Ie16bc5b3445ab75e97345d47e26e156ce815c578
    
    macloader: add new semcosh mac addresses
    
    Change-Id: Ia54f97011fe3966b60cff3a33f7fd885946438db

--- macloader/macloader.cpp
+++ macloader/macloader.cpp
@@ -87,7 +87,11 @@ int main() {
     }
 
     /* semcosh */
-    if (strncasecmp(mac_addr_half, "5c:0a:5b", 9) == 0 ||
+    if (strncasecmp(mac_addr_half, "34:23:ba", 9) == 0 ||
+        strncasecmp(mac_addr_half, "38:aa:3c", 9) == 0 ||
+        strncasecmp(mac_addr_half, "5c:0a:5b", 9) == 0 ||
+        strncasecmp(mac_addr_half, "88:32:9b", 9) == 0 ||
+        strncasecmp(mac_addr_half, "90:18:7c", 9) == 0 ||
         strncasecmp(mac_addr_half, "cc:3a:61", 9) == 0) {
         type = SEMCOSH;
     }
