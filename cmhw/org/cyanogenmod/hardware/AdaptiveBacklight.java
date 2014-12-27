6108a6b92231dfbfee76651b6a246822caec1980
repo Shareef Ali <shareefalis@gmail.com>
2014-12-26 23:40:22 -0600

    AdaptiveBacklight: fix illegal value exception.
    Change-Id: I8ea84451c8dd4be35a2d2e62547fc81a2b704dd1

--- cmhw/org/cyanogenmod/hardware/AdaptiveBacklight.java
+++ cmhw/org/cyanogenmod/hardware/AdaptiveBacklight.java
@@ -47,7 +47,7 @@ public class AdaptiveBacklight {
      * the operation failed while reading the status; true in any other case.
      */
     public static boolean isEnabled() {
-        if (Integer.parseInt(FileUtils.readOneLine(FILE_CABC)) == 1) {
+        if (TextUtils.equals(FileUtils.readOneLine(FILE_CABC), "1")) {
             return true;
         } else {
             return false;
