3a5ed01ec4e74db2aed7dfdee6166af3fd9450d1
Elliott Hughes <enh@google.com>
2015-01-09 15:27:28 -0800

    We have POSIX clocks.
    We're probably not running the RIL on a Mac.
    
    Change-Id: Ib6be56cd26d67e2b72b559cb364b1e34ab613016

--- ril/libril/ril_event.cpp
+++ ril/libril/ril_event.cpp
@@ -95,14 +95,10 @@ static void dump_event(struct ril_event * ev)
 
 static void getNow(struct timeval * tv)
 {
-#ifdef HAVE_POSIX_CLOCKS
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
     tv->tv_sec = ts.tv_sec;
     tv->tv_usec = ts.tv_nsec/1000;
-#else
-    gettimeofday(tv, NULL);
-#endif
 }
 
 static void init_list(struct ril_event * list)
