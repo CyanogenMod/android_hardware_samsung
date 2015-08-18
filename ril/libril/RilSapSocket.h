6eec81990db0e45d063a9733a64bb2b67e63734b
Pavel Zhamaitsiak <pavelz@google.com>
2015-08-18 11:40:01 -0700

    Fix memory leaks and use of uninitialized pointers.
    Bug: 23187886
    Change-Id: Ic15a02a902b591362b5c52c20afabe511fb0bbb7

--- ril/libril/RilSapSocket.h
+++ ril/libril/RilSapSocket.h
@@ -88,12 +88,6 @@ class RilSapSocket : public RilSocket {
         RIL_RadioFunctions *uimFuncs);
 
         /**
-         * Process requests from the dispatch request queue.
-         * @param Request to be dispatched.
-         */
-        int processRequest(MsgHeader *request);
-
-        /**
          * Ril envoronment variable that holds the request and
          * unsol response handlers.
          */
@@ -214,8 +208,8 @@ class RilSapSocket : public RilSocket {
         RIL_RadioFunctions *inputUimFuncs);
 
         /**
-         * Called by the processRequest method to dispatch the request to
-         * the lower layers. It calls the on request function.
+         * Dispatches the request to the lower layers.
+         * It calls the on request function.
          *
          * @param The request message.
          */
