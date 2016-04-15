2b45dcb1109e6c0ef35950bfbfab93a37482d309
Christopher N. Hesse <raymanfx@gmail.com>
2016-04-15 18:38:35 +0200

    ril: Restore alphabetical order for modem barrier
    Change-Id: Iae8f7cb6500c16d5b88d8e60d4e7e97ee6539a19

--- ril/Android.mk
+++ ril/Android.mk
@@ -20,7 +20,7 @@ ifeq ($(BOARD_VENDOR),samsung)
 
 # libril
 ifeq ($(BOARD_PROVIDES_LIBRIL),true)
-ifneq ($(filter xmm6260 xmm6262 xmm6360 xmm7260 m7450 ss333 mdm9x35 tss310,$(BOARD_MODEM_TYPE)),)
+ifneq ($(filter m7450 mdm9x35 ss333 tss310 xmm6260 xmm6262 xmm6360 xmm7260,$(BOARD_MODEM_TYPE)),)
 include $(RIL_PATH)/libril/Android.mk
 endif
 endif
