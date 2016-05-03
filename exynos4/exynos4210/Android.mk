08bb4a65001512ffb5cf78532857a88771a55e8e
Arnab Chaudhuri <coolguyforgenerationx@gmail.com>
2016-05-03 09:50:10 +0530

    liblights: Add BLN support
    Change-Id: Ibc23ebe7665bf9f0bc2d89b80938d6a14f1b00f0

--- exynos4/exynos4210/Android.mk
+++ exynos4/exynos4210/Android.mk
@@ -15,7 +15,12 @@
 #
 
 ifeq ($(TARGET_BOARD_PLATFORM),exynos4)
-exynos4210_dirs := liblights
+
+  ifeq ($(BOARD_HAS_BLN),true)
+    exynos4210_dirs := $(ANDROID_BUILD_TOP)/vendor/samsung/i9100/liblightsprebuilt
+  else
+    exynos4210_dirs := liblights
+  endif
 
 include $(call all-named-subdir-makefiles,$(exynos4210_dirs))
 
