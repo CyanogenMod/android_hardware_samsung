66c10c23720f7d1360c786de7d61507a04f7c74b
Arne Coucheron <arco68@gmail.com>
2016-07-13 07:05:45 +0200

    AdvancedDisplay: Fix build error
    Change-Id: Ic0bac704bcf0b9bbcc374765db7df8b339bd1d42

--- AdvancedDisplay/Android.mk
+++ AdvancedDisplay/Android.mk
@@ -7,6 +7,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := AdvancedDisplay
 LOCAL_CERTIFICATE := platform
+LOCAL_JAVA_LIBRARIES := org.cyanogenmod.platform.internal
 
 include $(BUILD_PACKAGE)
 
