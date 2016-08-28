022eadd45887b41cd87a6e9482f83c08c1abd081
Christopher N. Hesse <raymanfx@gmail.com>
2016-08-28 15:37:48 +0200

    libril: Export headers
    Change-Id: Ie61d8eb47f19fd825713357a538754f05b89d11a

--- ril/libril/Android.mk
+++ ril/libril/Android.mk
@@ -38,6 +38,8 @@ endif
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
 LOCAL_C_INCLUDES += external/nanopb-c
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/../include
 
 LOCAL_MODULE:= libril
 
