7cbebcf1e7ec8b7f425aeb1890b8019a33535c11
Christopher N. Hesse <raymanfx@gmail.com>
2016-12-07 12:12:23 +0100

    liblights: Expose panel brightness in helper lib
    Change-Id: I810166818eac84466dfe2c4b88dff47e83a181c0

---
+++ liblights/include/liblights/samsung_lights_helper.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SAMSUNG_LIGHTS_HELPER_H
+#define SAMSUNG_LIGHTS_HELPER_H
+
+#include <samsung_lights.h>
+
+/*
+ * Interfaces for other modules accessing panel brightness data.
+ * For documentation, see lights_helper.c
+ */
+inline int get_cur_panel_brightness();
+inline int get_max_panel_brightness();
+inline void set_cur_panel_brightness(const int brightness);
+inline void set_max_panel_brightness(const int brightness);
+
+#endif // SAMSUNG_LIGHTS_HELPER_H
